2268a92b66a86e8a893ca04f2647b3efb9c0b9d2
Sanjana Ramesh <95674920+Sanjana268@users.noreply.github.com>
2022-02-04 03:55:48 +0000

    p1 original

--- p1original.c
+++ p1original.c
@@ -1,2 +1,24 @@
+#include <stdio.h>
+int input(int *a, int *b)
+{
+  printf("Enter a,b value\n");
+  scanf("%d %d",a,b);
+}
 
+int add(int a, int b, int *sum)
+{
+  *sum=a+b;
+}
+int output(int c)
+{
+  printf("sum is %d", c);
+}
+
+int main()
+{
+  int x,y,z;
+  input(&x,&y);
+  add(x,y,&z);
+  output(z);
+  return 0;
 }
