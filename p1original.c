c78ec9357bda0b474e26e6b964bf77656008fe59
Sanjana Ramesh <95674920+Sanjana268@users.noreply.github.com>
2022-02-04 03:56:59 +0000

    p1original.c

--- p1original.c
+++ p1original.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-int input(int *a, int *b)
+void input(int *a, int *b)
 {
   printf("Enter a,b value\n");
   scanf("%d %d",a,b);
 }
 
-int add(int a, int b, int *sum)
+void add(int a, int b, int *sum)
 {
   *sum=a+b;
 }
-int output(int c)
+void output(int c)
 {
   printf("sum is %d", c);
 }
