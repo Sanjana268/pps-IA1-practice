c76f05446e8b2a3441fb3f744640adc721ce0f3e
Sanjana Ramesh <95674920+Sanjana268@users.noreply.github.com>
2022-02-05 03:03:15 +0000

    p4original.c

---
+++ p4original.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int input_array_size()
+{
+  int n;
+  printf("Enter the value of a number\n");
+  scanf("%d",&n);
+  return n;
+}
+
+void input_array(int n,int a[n])
+{
+  for(int i=0;i<n;i++)
+  {
+    printf("enter the input\n");
+    scanf("%d",&a[i]);
+
+  }
+}
+int sum_n_arrays(int n,int a[n])
+{
+  int sum=0;
+  for(int i=0;i<n;i++){
+    sum= sum+a[i];
+  }
+  return sum;
+}
+
+void output(int n,int a[n],int sum)
+{
+  printf("the sum of %d inputs of is %d\n",n,sum);
+
+}
+
+int main()
+{
+  int n,sum;
+  n=input_array_size();
+  int a[n];
+  input_array(n,a);
+  sum=sum_n_arrays(n,a);
+  output(n,a,sum);
+  return 0;
+}
